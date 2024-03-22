4310818e6c1e3ca3914af5b83e00913d3102c804
Rodrigo Acevedo <racevedo@mivest.io>
2024-03-22 16:59:01 -0600

    Second commit to BLoC

---
+++ lib/presentation/pages/portfolio/portfolio_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:stocks_tracking_app/models/position_model.dart';
+import 'package:stocks_tracking_app/providers/state_provider.dart';
+import 'package:stocks_tracking_app/widgets/side_menu.dart';
+
+class PortfolioScreen extends StatefulWidget {
+  static const String name = 'portfolio_screen';
+  static const String link = '/';
+
+  const PortfolioScreen({super.key});
+
+  @override
+  State<PortfolioScreen> createState() => _PortfolioScreenState();
+}
+
+class _PortfolioScreenState extends State<PortfolioScreen> {
+  final NumberFormat f = NumberFormat('#,##0.00', 'en_US');
+  bool isLoading = false;
+  ThemeData themeData = ThemeData();
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      isLoading = true;
+    });
+    context.read<StateProvider>().getPortfolio();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> onRefresh() async {
+    setState(() {
+      isLoading = true;
+    });
+    context.read<StateProvider>().getPortfolio();
+    await Future<void>.delayed(const Duration(seconds: 2));
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    themeData = Theme.of(context);
+
+    List<PositionModel> positions =
+        context.watch<StateProvider>().userData.portfolio;
+    double currentValue = context.watch<StateProvider>().userData.currentValue;
+    double initialInvestment =
+        context.watch<StateProvider>().userData.initialInvestment;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Stocks Tracking App',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: themeData.primaryColor,
+      ),
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        edgeOffset: 0,
+        strokeWidth: 2,
+        backgroundColor: themeData.primaryColor,
+        color: Colors.white,
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Skeletonizer(
+            enabled: isLoading,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$${f.format(currentValue)}',
+                  style: themeData.textTheme.headlineLarge,
+                ),
+                Text(
+                  '\$${f.format(currentValue - initialInvestment)} (${(((currentValue - initialInvestment) / initialInvestment) * 100).toStringAsFixed(2)}%)',
+                  style: themeData.textTheme.titleMedium,
+                ),
+                ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: positions.length,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Divider(
+                          height: 0,
+                          thickness: 1,
+                          color: themeData.primaryColor),
+                  itemBuilder: (context, index) {
+                    final position = positions[index];
+                    return _StockListItem(
+                        position: position, themeData: themeData);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      drawer: const SideMenu(
+        selectedMenu: 0,
+      ),
+    );
+  }
+}
+
+class _StockListItem extends StatelessWidget {
+  final PositionModel position;
+  final ThemeData themeData;
+  final NumberFormat f = NumberFormat('#,##0.00', 'en_US');
+  _StockListItem({required this.position, required this.themeData});
+
+  @override
+  Widget build(BuildContext context) {
+    final diferencePrice = position.lastPrice - position.averagePrice;
+    final pctGain =
+        (((position.lastPrice - position.averagePrice) / position.lastPrice) *
+            100);
+
+    return ListTile(
+      title: Text('${position.name} (${position.ticker})',
+          style: const TextStyle(fontWeight: FontWeight.w700)),
+      subtitle: Text('${position.quantity.toString()} stocks'),
+      leading: Image.network(
+        position.logoUrl,
+        height: 30,
+        width: 30,
+        fit: BoxFit.fitWidth,
+      ),
+      dense: false,
+      contentPadding: const EdgeInsets.only(left: 10.0, right: 5.0),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '\$${f.format(position.lastPrice)}',
+              ),
+              Text(
+                '${(diferencePrice > 0 ? '+' : '')}${f.format(diferencePrice)} (${(pctGain > 0 ? '+' : '')}${(pctGain).toStringAsFixed(2)})%',
+              ),
+            ],
+          ),
+          Icon(
+            Icons.arrow_forward_ios_rounded,
+            color: themeData.primaryColor,
+          )
+        ],
+      ),
+      onTap: () {
+        context.push('/stockdetails', extra: position);
+      },
+    );
+  }
+}
---
+++ lib/presentation/pages/screens.dart
@@ -0,0 +1,4 @@
+export 'package:stocks_tracking_app/presentation/pages/login/login_screen.dart';
+export 'package:stocks_tracking_app/presentation/pages/portfolio/portfolio_screen.dart';
+export 'package:stocks_tracking_app/presentation/pages/stock_details/stock_details_screen.dart';
+export 'package:stocks_tracking_app/presentation/pages/about/about_screen.dart';
